309ea16a1b1bdcc5de123d2daccfccfb955944dc
saxsir <saxsir.256@gmail.com>
2014-09-18 16:37:24 +0900

    zshrc.mineはローカルごとに固有の設定を書くように切り分け

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ if [ ! -e ~/.vim/bundle/neobundle.vim'' ]; then
 fi
 
 # update submodule
-echo "Updating oh-my-zsh..."
+echo "Updating submodules(oh-my-zsh, vimperator-plugins)..."
 git submodule init
 git submodule update
 if [ -e "$PWD/oh-my-zsh" ]; then
@@ -18,31 +18,29 @@ if [ -e "$PWD/oh-my-zsh" ]; then
   git checkout master
   cd ..
 fi
-
-echo "Updating vimperator-plugins"
 if [ -e "$PWD/vimperator/vimperator-plugins" ]; then
   cd vimperator/vimperator-plugins
   git checkout master
   cd ../..
 fi
 
-
 # create symbolic link
 echo "Creating symbolic links..."
 ln -Fis "$PWD/zshenv" ~/.zshenv
 ln -Fis "$PWD/zshrc" ~/.zshrc
-ln -Fis "$PWD/zshrc.mine" ~/.zshrc.mine
+cp -Fi  "$PWD/zshrc.mine.sample" ~/.zshrc.mine
 ln -Fis "$PWD/oh-my-zsh" ~/.oh-my-zsh
 ln -Fis "$PWD/gitignore_global" ~/.gitignore_global
 ln -Fis "$PWD/gitconfig" ~/.gitconfig
 ln -Fis "$PWD/vimrc" ~/.vimrc
 ln -Fis "$PWD/gemrc" ~/.gemrc
 ln -Fis "$PWD/vimperatorrc" ~/.vimperatorrc
-ln -Fis "$PWD/vimperator" ~/.vimperator
+if [ ! -e ~/.vimperator ]; then
+  mkdir -p ~/.vimperator
+fi
 if [ ! -e ~/.vimperator/plugin ]; then
   mkdir -p ~/.vimperator/plugin
 fi
 ln -Fis "$PWD/vimperator/colors" ~/.vimperator/colors
+ln -Fis "$PWD/vimperator/vimperator-plugins" ~/.vimperator/
 ln -Fis "$PWD/vimperator/vimperator-plugins/plugin_loader.js" ~/.vimperator/plugin/
-
-
