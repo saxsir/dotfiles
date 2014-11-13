77a22945019c165e64fed95c91d90e9988a40610
saxsir <saxsir.256@gmail.com>
2014-11-13 17:37:56 +0900

    linuxだったらvimperator系の設定ファイルはコピーしないようにした

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ dotfiles
 ========
 
 設定ファイルとかとか
+
+## インストール方法
+
+```sh
+
+  $ ./install.sh
+
+```
+
+### OS=mac をつけて実行するとvimperatorも入る
+
+```sh
+
+  $ ./install.sh OS=mac
+
+```
--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+OS="linux"
+
 cd $(dirname $0)
 
 # install neobundle.git
@@ -18,7 +20,7 @@ if [ -e "$PWD/oh-my-zsh" ]; then
   git checkout master
   cd ..
 fi
-if [ -e "$PWD/vimperator/vimperator-plugins" ]; then
+if test $OS = "mac" && [ -e "$PWD/vimperator/vimperator-plugins" ]; then
   cd vimperator/vimperator-plugins
   git checkout master
   cd ../..
@@ -39,16 +41,18 @@ ln -Fis "$PWD/gitignore_global" ~/.gitignore_global
 ln -Fis "$PWD/gitconfig" ~/.gitconfig
 ln -Fis "$PWD/vimrc" ~/.vimrc
 ln -Fis "$PWD/gemrc" ~/.gemrc
-ln -Fis "$PWD/vimperatorrc" ~/.vimperatorrc
-if [ ! -e ~/.vimperator ]; then
-  mkdir ~/.vimperator
+if test $OS = "mac"; then
+  ln -Fis "$PWD/vimperatorrc" ~/.vimperatorrc
+  if [ ! -e ~/.vimperator ]; then
+    mkdir ~/.vimperator
+  fi
+  if [ ! -e ~/.vimperator/plugin ]; then
+    mkdir ~/.vimperator/plugin
+  fi
+  ln -Fis "$PWD/vimperator/colors" ~/.vimperator/
+  ln -Fis "$PWD/vimperator/vimperator-plugins" ~/.vimperator/
+  ln -Fis "$PWD/vimperator/vimperator-plugins/plugin_loader.js" ~/.vimperator/plugin/
 fi
-if [ ! -e ~/.vimperator/plugin ]; then
-  mkdir ~/.vimperator/plugin
-fi
-ln -Fis "$PWD/vimperator/colors" ~/.vimperator/
-ln -Fis "$PWD/vimperator/vimperator-plugins" ~/.vimperator/
-ln -Fis "$PWD/vimperator/vimperator-plugins/plugin_loader.js" ~/.vimperator/plugin/
 if [ ! -e ~/.vim ]; then
   mkdir ~/.vim
 fi
