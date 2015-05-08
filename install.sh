57171edf375bdfd7e678678bd61a5855d45f35eb
saxsir <saxsir.256+github@gmail.com>
2015-05-08 14:16:21 +0900

    ファイル構成の変更に伴うインストールスクリプトの修正

--- install.sh
+++ install.sh
@@ -1,47 +1,62 @@
 #!/bin/sh
 
 OS="linux"
-
 cd $(dirname $0)
 
-# install neobundle.git
-if [ ! -e ~/.vim/bundle/neobundle.vim'' ]; then
-  echo "Installing neobundle.vim..."
-  mkdir -p ~/.vim/bundle
-  git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+# ==================
+# Install submodules
+# ==================
+git submodule init oh-my-zsh
+git submodule init neobundle.vim
+if [ $OS = "mac" ]; then
+  git submodule init vimperator/vimperator-plugins
 fi
-
-# update submodule
-echo "Updating submodules(oh-my-zsh, vimperator-plugins, zsh-completions)..."
-git submodule init
 git submodule update
+
 if [ -e "$PWD/oh-my-zsh" ]; then
   cd oh-my-zsh
   git checkout master
   cd ..
 fi
-if test $OS = "mac" && [ -e "$PWD/vimperator/vimperator-plugins" ]; then
+if [ -e "$PWD/neobundle.vim" ]; then
+    cd neobundle.vim
+    git checkout master
+    cd ..
+fi
+if [ $OS = "mac" ]; then
   cd vimperator/vimperator-plugins
   git checkout master
   cd ../..
 fi
-if [ -e "$PWD/zsh-completions" ]; then
-  cd zsh-completions
-  git checkout master
-  cd ..
-fi
 
-# create symbolic link
-echo "Creating symbolic links..."
+# =====================
+# Create symbolic links
+# =====================
+# git
+cp ~/dotfiles/gitconfig.sample ./gitconfig
+sed -i -e 's/\<your name\>/saxsir/' ./gitconfig
+sed -i -e 's/\<your email\>/saxsir.256+github@gmail.com/' ./gitconfig
+ln -Fis "$PWD/gitconfig" ~/.gitconfig
+ln -Fis "$PWD/gitignore_global" ~/.gitignore_global
+
+# zsh
 ln -Fis "$PWD/zshenv" ~/.zshenv
 ln -Fis "$PWD/zshrc" ~/.zshrc
 cp -fi  "$PWD/zshrc.mine.sample" ~/.zshrc.mine
 ln -Fis "$PWD/oh-my-zsh" ~/.oh-my-zsh
-ln -Fis "$PWD/gitignore_global" ~/.gitignore_global
-# ln -Fis "$PWD/gitconfig" ~/.gitconfig
+
+# vim
+mkdir -p ~/.vim/bundle
 ln -Fis "$PWD/vimrc" ~/.vimrc
+ln -Fis "$PWD/neobundle.vim" ~/.vim/bundle/neobundle.vim
+
+# gem
 ln -Fis "$PWD/gemrc" ~/.gemrc
-if test $OS = "mac"; then
+
+# ==========================
+# Vimperator(Macintosh only)
+# ==========================
+if [ $OS = "mac" ]; then
   ln -Fis "$PWD/vimperatorrc" ~/.vimperatorrc
   if [ ! -e ~/.vimperator ]; then
     mkdir ~/.vimperator
@@ -53,13 +68,3 @@ if test $OS = "mac"; then
   ln -Fis "$PWD/vimperator/vimperator-plugins" ~/.vimperator/
   ln -Fis "$PWD/vimperator/vimperator-plugins/plugin_loader.js" ~/.vimperator/plugin/
 fi
-if [ ! -e ~/.vim ]; then
-  mkdir ~/.vim
-fi
-ln -Fis "$PWD/vim/dict" ~/.vim/
-ln -Fis "$PWD/vim/conf.d" ~/.vim/
-
-if [ ! -e ~/.zsh ]; then
-  mkdir ~/.zsh
-fi
-ln -Fis "$PWD/zsh-completions" ~/.zsh
